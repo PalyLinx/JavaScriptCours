d86da888c2b862895a76030a82170fd972c16175
Paly Linx <linxpaly@gmail.com>
2025-08-29 23:50:09 +0100

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,8 +1,34 @@
 let myName ="MOHAMED";
-
 const pi=3.14;
 
 console log(myName);
  
 console log(pi);
 
+let name="Lahcene";  // String
+
+console log(typeof name); 
+
+let age=30;  // Number
+
+console log(typeof age);
+
+
+let isAccepted= false; // boolean
+
+console log(typeof isAccepted);
+
+
+let firstName= undefined; // boolean
+
+console log(typeof firstName);
+
+let age=30;  // Number
+
+let age=null;
+
+console log(typeof age);
+
+let age=undefined;  // Number
+
+console log(typeof age);
